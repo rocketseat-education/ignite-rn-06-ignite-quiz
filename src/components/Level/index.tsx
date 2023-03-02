e3a1fc7a3021aa08152dcf1ca024369c9195f69d
RennanD <rplaykl@gmail.com>
2023-03-02 11:52:46 -0300

    Animações - Os pilares do reanimated

--- src/components/Level/index.tsx
+++ src/components/Level/index.tsx
@@ -1,4 +1,8 @@
 import { TouchableOpacity, TouchableOpacityProps, Text, View } from 'react-native';
+import Animated, { 
+  useSharedValue, 
+  useAnimatedStyle,
+} from 'react-native-reanimated';
 
 import { THEME } from '../../styles/theme';
 import { styles } from './styles';
@@ -17,13 +21,22 @@ type Props = TouchableOpacityProps & {
 
 export function Level({ title, type = 'EASY', isChecked = false, ...rest }: Props) {
 
+  const scale = useSharedValue(1);
+
   const COLOR = TYPE_COLORS[type];
+
+  const animatedContainerStyle = useAnimatedStyle(() => {
+    return {
+      transform: [{ scale: scale.value }]
+    }
+  })
 
   return (
     <TouchableOpacity {...rest}>
-      <View style={
+      <Animated.View style={
         [
           styles.container,
+          animatedContainerStyle,
           { borderColor: COLOR, backgroundColor: isChecked ? COLOR : 'transparent' }
         ]
       }>
@@ -34,7 +47,7 @@ export function Level({ title, type = 'EASY', isChecked = false, ...rest }: Prop
           ]}>
           {title}
         </Text>
-      </View>
+      </Animated.View>
     </TouchableOpacity>
   );
 }
