8ffe2348da32577de9448f6c6255187d6349fbe2
RennanD <rplaykl@gmail.com>
2023-03-02 16:49:23 -0300

    Animações - Keyframes

--- src/components/Question/index.tsx
+++ src/components/Question/index.tsx
@@ -1,4 +1,6 @@
-import { View, Text } from 'react-native';
+import { Dimensions, Text } from 'react-native';
+import Animated, { Keyframe } from 'react-native-reanimated';
+
 
 import { Option } from '../Option';
 import { styles } from './styles';
@@ -14,9 +16,52 @@ type Props = {
   setAlternativeSelected?: (value: number) => void;
 }
 
+const SCREEN_WIDTH = Dimensions.get('window').width
+
 export function Question({ question, alternativeSelected, setAlternativeSelected }: Props) {
+  const enteringKeyFrame = new Keyframe({
+    0: {
+      opacity: 0,
+      transform: [
+        { translateX: SCREEN_WIDTH },
+        { rotate: '90deg' }
+      ]
+    },
+    70: {
+      opacity: 0.3
+    },
+    100: {
+      opacity: 1,
+      transform: [
+        { translateX: 0},
+        { rotate: '0deg' }
+      ]
+    }
+  })
+
+  const exitingKeyFrame = new Keyframe({
+    from: {
+      opacity: 1,
+      transform: [
+        { translateX: 0 },
+        { rotate: '0deg'}
+      ]
+    },
+    to: {
+      opacity: 0,
+      transform: [
+        { translateX: SCREEN_WIDTH * (-1) },
+        { rotate: '-90deg'}
+      ]
+    }
+  })
+  
   return (
-    <View style={styles.container}>
+    <Animated.View
+      style={styles.container}
+      entering={enteringKeyFrame}
+      exiting={exitingKeyFrame}
+    >
       <Text style={styles.title}>
         {question.title}
       </Text>
@@ -31,6 +76,6 @@ export function Question({ question, alternativeSelected, setAlternativeSelected
           />
         ))
       }
-    </View>
+    </Animated.View>
   );
 }
