2d8ec9723d3024afc8414c5451d42d07cffe31dd
RennanD <rplaykl@gmail.com>
2023-03-06 16:26:51 -0300

    Microinterações - Detectando Gestos

--- src/screens/Quiz/index.tsx
+++ src/screens/Quiz/index.tsx
@@ -10,8 +10,10 @@ import Animated, {
   interpolate,
   Extrapolate,
   Easing,
-  useAnimatedScrollHandler
+  useAnimatedScrollHandler,
+  event
 } from 'react-native-reanimated';
+import { GestureDetector, Gesture } from 'react-native-gesture-handler';
 
 import { styles } from './styles';
 import { THEME } from '../../styles/theme';
@@ -41,6 +43,7 @@ export function Quiz() {
 
   const shake = useSharedValue(0);
   const scrollY = useSharedValue(0);
+  const cardPosition = useSharedValue(0);
 
   const { navigate } = useNavigation();
 
@@ -153,6 +156,15 @@ export function Quiz() {
     }
   })
 
+  const onPan =  Gesture
+  .Pan()
+  .onUpdate((event) => {
+    cardPosition.value = event.translationX
+  })
+  .onEnd(() => {
+    cardPosition.value = withTiming(0)
+  })
+
   useEffect(() => {
     const quizSelected = QUIZ.filter(item => item.id === id)[0];
     setQuiz(quizSelected);
@@ -187,14 +199,17 @@ export function Quiz() {
         </Animated.View>
         
 
-        <Animated.View style={shakeStyleAnimated}>
-          <Question
-            key={quiz.questions[currentQuestion].title}
-            question={quiz.questions[currentQuestion]}
-            alternativeSelected={alternativeSelected}
-            setAlternativeSelected={setAlternativeSelected}
-          />
-        </Animated.View>
+        <GestureDetector gesture={onPan}>
+          <Animated.View style={shakeStyleAnimated}>
+            <Question
+              key={quiz.questions[currentQuestion].title}
+              question={quiz.questions[currentQuestion]}
+              alternativeSelected={alternativeSelected}
+              setAlternativeSelected={setAlternativeSelected}
+            />
+          </Animated.View>
+        </GestureDetector>
+        
 
         <View style={styles.footer}>
           <OutlineButton title="Parar" onPress={handleStop} />
