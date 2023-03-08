39cb3843bc3549b4fcca08fbab64943c78065b8a
RennanD <rplaykl@gmail.com>
2023-03-08 17:13:53 -0300

    Feedback Visual - Criando elemento com Skia

--- src/components/Option/index.tsx
+++ src/components/Option/index.tsx
@@ -1,13 +1,27 @@
 import { Text, TouchableOpacity, TouchableOpacityProps } from 'react-native';
+import {
+  Canvas,
+  Path,
+  Skia
+} from '@shopify/react-native-skia';
 
 import { styles } from './styles';
+import { THEME } from '../../styles/theme';
 
 type Props = TouchableOpacityProps & {
   checked: boolean;
   title: string;
 }
 
+const CHECK_SIZE = 28;
+const CHECK_STROKE = 2;
+
 export function Option({ checked, title, ...rest }: Props) {
+  const RADIUS = (CHECK_SIZE - CHECK_STROKE) / 2;
+
+  const path = Skia.Path.Make();
+  path.addCircle(CHECK_SIZE, CHECK_SIZE, RADIUS)
+
   return (
     <TouchableOpacity
       style={
@@ -21,6 +35,24 @@ export function Option({ checked, title, ...rest }: Props) {
       <Text style={styles.title}>
         {title}
       </Text>
+
+      <Canvas style={{ height: CHECK_SIZE * 2, width: CHECK_SIZE * 2 }}>
+        <Path 
+          path={path}
+          color={THEME.COLORS.GREY_500}
+          style="stroke"
+          strokeWidth={CHECK_STROKE}
+        /> 
+
+        <Path 
+          path={path}
+          color={THEME.COLORS.BRAND_LIGHT}
+          style="stroke"
+          strokeWidth={CHECK_STROKE}
+          start={0}
+          end={0.5}
+        /> 
+      </Canvas>
     </TouchableOpacity>
   );
 }
